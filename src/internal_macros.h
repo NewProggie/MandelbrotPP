c3f12f15066cae5d5a874f0e8b68f63904bc08e5
Kai Wolf <kai.wolf@gmail.com>
2016-07-23 10:20:20 +0200

    Remove IS_DEFINED

--- src/internal_macros.h
+++ src/internal_macros.h
@@ -2,36 +2,16 @@
 // Use of this source code is governed by a MIT license that can be found in
 // the LICENSE file.
 
-// Convenient macro check for definitions, initially taken from:
-// https://plus.google.com/+LinusTorvalds/posts/9gntjh57dXt
-// Following macro expands to 1 if a preprocessor value was defined to 1 and 0
-// if it wasn't or defined to another value. May be used directory as:
-//
-// #if IS_DEFINED(SOMETHING)
-// ...
-// #endif
-//
-// or even directly in the code, which looks much better:
-//
-// if (IS_DEFINED(SOMETHING)) {
-// ...
-// }
-#define IS_DEFINED(macro) IS_DEFINED_(macro)
-#define MACROTEST_1 ,
-#define IS_DEFINED_(value) IS_DEFINED__(MACROTEST_##value)
-#define IS_DEFINED__(comma) IS_DEFINED___(comma 1, 0)
-#define IS_DEFINED___(_, v, ...) v
-
 // determine host os system
 #if defined(_WIN32)
-# define MANDELBROTPP_OS_WINDOWS 1
+# define OS_WINDOWS 1
 # define __func__ __FUNCTION__
 #elif defined(__APPLE__)
-# define MANDELBROTPP_OS_MACOSX 1
+# define OS_MACOSX 1
 #elif defined(__FreeBSD__)
-# define MANDELBROTPP_OS_FREEBSD 1
+# define OS_FREEBSD 1
 #elif defined(__linux__)
-# define MANDELBROTPP_OS_LINUX 1
+# define OS_LINUX 1
 #endif
 
 // determine used compiler
